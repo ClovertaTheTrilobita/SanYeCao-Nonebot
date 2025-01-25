84a0a2107142e94bf660aeec0bc0f6d2953ca524
ClovertaTheTrilobita <cloverta@petalmail.com>
2025-01-25 13:55:34 +0800

    新增错误日志功能 update .gitignore

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 import nonebot
 from nonebot.adapters.qq import Adapter as QQAdapter
+from nonebot import logger
+from nonebot.log import default_format, default_filter
 
 nonebot.init()
 driver = nonebot.get_driver()
 driver.register_adapter(QQAdapter)  # 注册QQ适配器
 nonebot.load_from_toml("pyproject.toml")
+logger.add("error.log", level="ERROR", format=default_format, rotation="1 week")
 
 from src.my_sqlite.data_init.db_connect import disconnect, init
 driver.on_startup(init)
