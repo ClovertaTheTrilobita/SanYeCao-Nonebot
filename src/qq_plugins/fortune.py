213ba24a9b5d9eee29e6cc9dc972b5f2b98b0390
SlyAimer <2289782085@qq.com>
2025-02-03 22:23:26 +0800

    今日塔罗提交,并删除冗余文件

--- src/qq_plugins/fortune.py
+++ src/qq_plugins/fortune.py
@@ -12,14 +12,7 @@ fortune_by_sqlite = on_command("今日运势", rule=to_me(), priority=10, block=
 async def get_today_fortune(message: MessageEvent):
 
     local_image_path = get_image_names()
-    member_openid = message.get_user_id()
-    # 查询今日是否已经获取过今日运势，如果获取过则直接从日志取
-    result = await QrFortuneLog.is_get_fortune_log(member_openid)
-    if result is None:
-        # 获取 运势说明
-        result = await QrFortune.get_fortune()
-        # 把抽取的今日运势插入日志
-        await QrFortuneLog.insert_fortune_log(result, member_openid)
+    result = await QrFortune.get_fortune(message.get_user_id())
 
     content = ("\n" + "您的今日运势为：" + "\n" +
                result.fortune_summary + "\n" +
