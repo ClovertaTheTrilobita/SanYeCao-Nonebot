94df2c765313ca383bd55535f24dfbe3829625ff
SlyAimer <2289782085@qq.com>
2025-02-06 22:42:34 +0800

    图文合成方式修改

--- src/configs/path_config.py
+++ src/configs/path_config.py
@@ -4,15 +4,22 @@ from pathlib import Path
 path = os.getcwd()+'/src/resources/image'
 
 
-#塔罗牌图片路径
+# 塔罗牌图片路径
 image_local_qq_image_path =  path+'/qq_image'
-#个人图片路径
+# 个人图片路径
 image_local_path= path+"/MaoYuNa"
-#塔罗牌图片路径
+# 塔罗牌图片路径
 tarots_img_path = path+'/tarot/TarotImages/'
-#摸摸头图片路径
+# 摸摸头图片路径
 rua_png = path+'/rua/'
+# 喜报、悲报图片路径
+good_bad = path+'/good_bad_news/'
 
+# 字体路径
+font_path = path+'/font/'
+
+# 临时数据路径
+temp_path = os.getcwd()+'/src/resources/temp/'
 
 
 # 图片路径
@@ -21,14 +28,8 @@ IMAGE_PATH = Path() / "resources" / "image"
 RECORD_PATH = Path() / "resources" / "record"
 # 文本路径
 TEXT_PATH = Path() / "resources" / "text"
-# 日志路径
-LOG_PATH = Path() / "log"
-# 字体路径
-FONT_PATH = Path() / "resources" / "font"
 # 数据路径
 DATA_PATH = Path() / "data"
-# 临时数据路径
-TEMP_PATH = Path() / "resources" / "temp"
 # 网页模板路径
 TEMPLATE_PATH = Path() / "resources" / "template"
 
--- src/plugins/good_bad_news.py
+++ src/plugins/good_bad_news.py
@@ -2,43 +2,56 @@ from pathlib import Path
 from nonebot.rule import to_me
 from nonebot.plugin import on_command
 from nonebot.adapters.qq import Message, MessageEvent, MessageSegment, exception
-import urllib.parse, requests
+import urllib.parse
+import requests
 import time
 import httpx
+from src.clover_image.get_image import add_text_to_image
+from src.configs.path_config import font_path,good_bad,temp_path
 
-good_news = on_command("喜报", rule=to_me(), priority=10, block=True, aliases={"悲报"})
-@good_news.handle()
-async def good_news_img(message: MessageEvent):
-    if message.get_plaintext().startswith("/喜报"):
-        content = message.get_plaintext().replace("/喜报", "").strip()
-        url = "https://cdn.uuuix.com/api/v1/xbs/xb.php?"
-    else:
-        content = message.get_plaintext().replace("/悲报", "").strip()
-        url = "https://cdn.uuuix.com/api/v1/xbs/biob.php?"
-
-    params = {
-        'msg': content
-    }
-
-    await good_news.send("图片绘制中，请稍后~\n技术支持: JianDan大佬\nwww·uuuix·com")
-
-    query = urllib.parse.urlencode(params)
-    response = requests.get(url + query).json()
-
-    if response['code'] != 1:
-        await good_news.finish("请输入 /喜(悲)报+内容 哦。")
-
-    img_url = response['url']
+# good_news = on_command("喜报", rule=to_me(), priority=10, block=True, aliases={"悲报"})
+# @good_news.handle()
+# async def good_news_img(message: MessageEvent):
+#     if message.get_plaintext().startswith("/喜报"):
+#         content = message.get_plaintext().replace("/喜报", "").strip()
+#         url = "https://cdn.uuuix.com/api/v1/xbs/xb.php?"
+#     else:
+#         content = message.get_plaintext().replace("/悲报", "").strip()
+#         url = "https://cdn.uuuix.com/api/v1/xbs/biob.php?"
+#
+#     params = {
+#         'msg': content
+#     }
+#
+#     await good_news.send("图片绘制中，请稍后~\n技术支持: JianDan大佬\nwww·uuuix·com")
+#
+#     query = urllib.parse.urlencode(params)
+#     response = requests.get(url + query).json()
+#
+#     if response['code'] != 1:
+#         await good_news.finish("请输入 /喜(悲)报+内容 哦。")
+#
+#     img_url = response['url']
     # try:
     #     await good_news.finish(MessageSegment.clover_image(img_url))
     # except BaseException:
     #     await good_news.finish("出错啦，请重试。")
 
-    try:
-        await good_news.finish(MessageSegment.image(img_url))
-    except exception.ActionFailed as e:
-        print("\033[32m" + str(time.strftime("%m-%d %H:%M:%S")) +
-              "\033[0m [" + "\033[31;1mFAILED\033[0m" + "]" +
-              "\033[31;1m nonebot.adapters.qq.exception.ActionFailed \033[0m" + str(e))
-        await good_news.finish("图片发送失败，请重试。这绝对不是咱的错，绝对不是！")
 
+good_news = on_command("喜报", rule=to_me(), priority=10, block=True, aliases={"悲报"})
+@good_news.handle()
+async def function(message: MessageEvent):
+    value = message.get_plaintext().split(" ")
+    keyword,content  = value[0], value[1]
+    if value[1] == "":
+        await good_news.finish("请输入 /喜(悲)报+内容 哦。")
+    if keyword == "/喜报":
+        await add_text_to_image(image_path=good_bad + "good_news.png", output_path=temp_path+"good_news.png", content=content,
+                                font_path=font_path + "msyh.ttc", font_size=64, text_color=(255, 0, 0),
+                                position="center")
+        await good_news.finish(MessageSegment.file_image(Path(temp_path+"good_news.png")))
+    elif keyword == "/悲报":
+        await add_text_to_image(image_path=good_bad + "bad_news.png", output_path=temp_path+"bad_news.png", content=content,
+                                font_path=font_path + "msyh.ttc", font_size=64, text_color=(128, 128, 128),
+                                position="center")
+        await good_news.finish(MessageSegment.file_image(Path(temp_path+"bad_news.png")))
