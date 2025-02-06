18bf5b6d4bbeef834d9342c1a1ef55d58066e4a5
SlyAimer <2289782085@qq.com>
2025-02-06 22:54:29 +0800

    路径文件修改

--- src/configs/path_config.py
+++ src/configs/path_config.py
@@ -1,45 +1,45 @@
 import os
 from pathlib import Path
 
-path = os.getcwd()+'/src/resources/image'
+path = os.getcwd()+'/src/resources'
 
 
 # 塔罗牌图片路径
-image_local_qq_image_path =  path+'/qq_image'
+image_local_qq_image_path =  path+'/image/qq_image'
 # 个人图片路径
-image_local_path= path+"/MaoYuNa"
+image_local_path= path+"/image/MaoYuNa"
 # 塔罗牌图片路径
-tarots_img_path = path+'/tarot/TarotImages/'
+tarots_img_path = path+'/image/tarot/TarotImages/'
 # 摸摸头图片路径
-rua_png = path+'/rua/'
+rua_png = path+'/image/rua/'
 # 喜报、悲报图片路径
-good_bad = path+'/good_bad_news/'
+good_bad = path+'/image/good_bad_news/'
 
 # 字体路径
-font_path = path+'/font/'
+font_path = path + '/font/'
 
 # 临时数据路径
-temp_path = os.getcwd()+'/src/resources/temp/'
+temp_path = path + '/temp/'
 
-
-# 语音路径
-RECORD_PATH = Path() / "src" / "resources" / "record"
-# 文本路径
-TEXT_PATH = Path() / "src" / "resources" / "text"
 # 日志路径
-LOG_PATH = Path() / "src" / "log"
-# 字体路径
-FONT_PATH = Path() / "src" / "resources" / "font"
-# 数据路径
-DATA_PATH = Path() / "src" / "data"
-# 临时数据路径
-TEMP_PATH = Path() / "src" / "resources" / "temp"
-# 网页模板路径
-TEMPLATE_PATH = Path() / "src" / "resources" / "template"
+log_path = path+'/log/'
+
 # 视频路径
-VIDEO_PATH = Path() / "src" / "resources" / "videos"
+video_path = path+'/video/'
 
 
+
+
+# # 语音路径
+# RECORD_PATH = Path() / "src" / "resources" / "record"
+# # 文本路径
+# TEXT_PATH = Path() / "src" / "resources" / "text"
+# # 数据路径
+# DATA_PATH = Path() / "src" / "data"
+# # 网页模板路径
+# TEMPLATE_PATH = Path() / "src" / "resources" / "template"
+# # 视频路径
+# VIDEO_PATH = Path() / "src" / "resources" / "videos"
 # IMAGE_PATH.mkdir(parents=True, exist_ok=True)
 # RECORD_PATH.mkdir(parents=True, exist_ok=True)
 # TEXT_PATH.mkdir(parents=True, exist_ok=True)
