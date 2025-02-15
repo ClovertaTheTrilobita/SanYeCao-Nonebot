1c74fe30b5c97c39f1098712ce4f26bd62968d74
SlyAimer <2289782085@qq.com>
2025-02-15 14:50:26 +0800

    feat(plugins/check.py): 添加重启功能
    在菜单中添加重启命令，并实现重启逻辑，包括权限检查和执行远程代码更新脚本。

--- src/plugins/check.py
+++ src/plugins/check.py
@@ -1,3 +1,4 @@
+import os
 import random
 from nonebot import  on_message
 from nonebot.rule import Rule, to_me
@@ -7,7 +8,7 @@ from src.clover_openai import ai_chat
 from src.clover_sqlite.models.chat import GroupChatRole
 from src.clover_sqlite.models.user import UserList
 
-menu = ['/今日运势','/今日塔罗','/图','/日报','/点歌','/摸摸头','/群老婆','/今日老婆', "/开启ai","/关闭ai","/角色列表","/添加人设", "/更新人设", "/删除人设", "/切换人设", "/管理员注册",
+menu = ['/重启','/今日运势','/今日塔罗','/图','/日报','/点歌','/摸摸头','/群老婆','/今日老婆', "/开启ai","/关闭ai","/角色列表","/添加人设", "/更新人设", "/删除人设", "/切换人设", "/管理员注册",
         '/待办', '/test','/天气','我喜欢你', "❤", "/待办查询", "/新建待办", "/删除待办" ,"/cf","/B站搜索", "/BV搜索", "/喜报", "/悲报", "/luxun","/鲁迅说",
         "/奶龙", "/repo", "/info", "/menu"]
 
@@ -59,6 +60,20 @@ async def send_menu_list():
     for command in send_menu:
         content += command + "\n"
     await get_menu.finish(content)
+
+restart = on_command("重启", rule=to_me(), priority=10, block=True)
+@restart.handle()
+async def handle_function(message: MessageEvent):
+
+    member_openid, group_openid = message.author.id, message.group_openid
+    if not await GroupChatRole.get_admin_list(group_openid, member_openid):
+        await restart.finish("您没有权限使用此功能。")
+
+    return_code = os.system("python ./update_remote_code.py")
+    if return_code == 0:
+        await restart.finish("重启成功")
+    else:
+        await restart.finish("重启失败")
 
 love = on_keyword({"我喜欢你", "❤"}, rule=to_me(), priority=2, block=False)
 @love.handle()
